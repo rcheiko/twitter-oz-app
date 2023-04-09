9a3d96caa61f7c9c58359c36cbcb1f67c2d2d8b2
Romain Cheiko <rcheiko@student.42nice.fr>
2023-04-09 18:26:31 +0200

    added route

--- src/path.tsx
+++ src/path.tsx
@@ -1,22 +1,31 @@
 export enum Route {
     DASHBOARD = 'DASHBOARD',
     PROFILE = 'PROFILE',
+    MESSAGES = 'MESSAGES',
+    BOOKMARKS = 'BOOKMARKS',
+    NOTIFICATIONS = 'NOTIFICATIONS',
     SETTINGS = 'SETTINGS',
     ERROR_PAGE = 'ERROR_PAGE',
   }
   
   const Routes = {
     [Route.DASHBOARD]: () => '/' as const,
-    [Route.ERROR_PAGE]: () => `/error` as const,
     [Route.PROFILE]: ({ profileName }: { profileName: string }) => `/${profileName}` as const,
+    [Route.MESSAGES]: () => `/messages` as const,
+    [Route.BOOKMARKS]: () => `/bookmarks` as const,
+    [Route.NOTIFICATIONS]: () => `/notifications` as const,
     [Route.SETTINGS]: () => `/settings` as const,
+    [Route.ERROR_PAGE]: () => `/error` as const,
   }
   
   const RouterRoutes = {
     [Route.DASHBOARD]: '/' as const,
-    [Route.ERROR_PAGE]: `/error` as const,
     [Route.PROFILE]: `/:profileName` as const,
+    [Route.MESSAGES]: `/messages` as const,
+    [Route.BOOKMARKS]: `/bookmarks` as const,
+    [Route.NOTIFICATIONS]: `/notifications` as const,
     [Route.SETTINGS]: `/settings` as const,
+    [Route.ERROR_PAGE]: `/error` as const,
   }
   
   export const getRouterRoutePath =
